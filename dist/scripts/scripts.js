a4c04e4a6def4fa771635ed7454db19c7c0d2065
immichelle <ieltsfnf@gmail.com>
2018-09-02 09:22:42 -0700

    added 15 minute increments, color variables, and made buttons more reusable

--- dist/scripts/scripts.js
+++ dist/scripts/scripts.js
@@ -1,12 +1,21 @@
 'use strict';
 
 var fn = function fn() {
+	// adding p tag for minus
 	var minus = document.getElementsByClassName('minus');
 
 	for (var i = 0; i < minus.length; i++) {
 		var e = document.createElement('p');
 		e.innerHTML = '-';
 		minus[i].appendChild(e);
+	}
+
+	//adding p tag for plus
+	var plus = document.getElementsByClassName('plus');
+	for (var i = 0; i < plus.length; i++) {
+		var e = document.createElement('p');
+		e.innerHTML = '+';
+		plus[i].appendChild(e);
 	}
 };
 
